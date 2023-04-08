fc118c7129517cccae598616bdb70bb5469e9414
gabrielyin <2021gabrielyin@gmail.com>
2023-04-07 21:43:05 -0300

    login/register page with react/tailwind

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+import Register from './pages/Register'
+import Login from './pages/Login'
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
+  )
+}
+
+export default App
